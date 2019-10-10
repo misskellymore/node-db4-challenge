87f06f57a3e4ca9b085f91929f7560a2d7eab209
Kelly Moreira <moreira.kd@outlook.com>
2019-10-10 16:24:36 -0400

    knex config

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,19 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+      filename: './db/node4'
+    },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+    useNullAsDefault: true,
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
+    },
+
+    seeds: {
+      directory: './db/seeds'
     }
-  }
+    
+  },  
 
 };
